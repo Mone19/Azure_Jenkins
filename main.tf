14550127ae80a471005eecc7f10a136102820742
Mone19 <fuocoangelos@yahoo.de>
2024-08-31 16:03:49 +0200

    testing

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.0.1"
     }
   }
@@ -11,41 +11,47 @@ provider "azurerm" {
   features {}
 }
 
+# Random Name Generator
+resource "random_pet" "resource_name" {
+  length    = 2
+  separator = "-"
+}
+
 variable "prefix" {
   default = "tfvmex"
 }
 
-# Resource group
-resource "azurerm_resource_group" "jenkins-rg" {
-  name     = "${var.prefix}-resources"
+# Resource Group
+resource "azurerm_resource_group" "jenkins_rg" {
+  name     = "${var.prefix}-${random_pet.resource_name.id}-resources"
   location = "West Europe"
 }
 
 # Virtual Network
-resource "azurerm_virtual_network" "jenkins-vmnet" {
-  name                = "${var.prefix}-network"
+resource "azurerm_virtual_network" "jenkins_vmnet" {
+  name                = "${var.prefix}-${random_pet.resource_name.id}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.jenkins-rg.location
-  resource_group_name = azurerm_resource_group.jenkins-rg.name
+  location            = azurerm_resource_group.jenkins_rg.location
+  resource_group_name = azurerm_resource_group.jenkins_rg.name
 }
 
 # Subnet
-resource "azurerm_subnet" "jenkins-subnet" {
-  name                 = "jenkins-subnet"
-  resource_group_name  = azurerm_resource_group.jenkins-rg.name
-  virtual_network_name = azurerm_virtual_network.jenkins-vmnet.name
+resource "azurerm_subnet" "jenkins_subnet" {
+  name                 = "${random_pet.resource_name.id}-subnet"
+  resource_group_name  = azurerm_resource_group.jenkins_rg.name
+  virtual_network_name = azurerm_virtual_network.jenkins_vmnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Network Security Group
-resource "azurerm_network_security_group" "jenkins-vmnsg" {
-  name                = "${var.prefix}-nsg"
-  location            = azurerm_resource_group.jenkins-rg.location
-  resource_group_name = azurerm_resource_group.jenkins-rg.name
+resource "azurerm_network_security_group" "jenkins_vmnsg" {
+  name                = "${var.prefix}-${random_pet.resource_name.id}-nsg"
+  location            = azurerm_resource_group.jenkins_rg.location
+  resource_group_name = azurerm_resource_group.jenkins_rg.name
 
   # Inbound rules
   security_rule {
-    name                       = "AllowSSH"
+    name                       = "${random_pet.resource_name.id}-AllowSSH"
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
@@ -57,7 +63,7 @@ resource "azurerm_network_security_group" "jenkins-vmnsg" {
   }
 
   security_rule {
-    name                       = "AllowHTTP"
+    name                       = "${random_pet.resource_name.id}-AllowHTTP"
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
@@ -69,7 +75,7 @@ resource "azurerm_network_security_group" "jenkins-vmnsg" {
   }
 
   security_rule {
-    name                       = "AllowJenkins"
+    name                       = "${random_pet.resource_name.id}-AllowJenkins"
     priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
@@ -82,31 +88,31 @@ resource "azurerm_network_security_group" "jenkins-vmnsg" {
 }
 
 # Network Interface
-resource "azurerm_network_interface" "jenkins-vmnic" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.jenkins-rg.location
-  resource_group_name = azurerm_resource_group.jenkins-rg.name
+resource "azurerm_network_interface" "jenkins_vmnic" {
+  name                = "${var.prefix}-${random_pet.resource_name.id}-nic"
+  location            = azurerm_resource_group.jenkins_rg.location
+  resource_group_name = azurerm_resource_group.jenkins_rg.name
 
   ip_configuration {
     name                          = "configuration"
-    subnet_id                     = azurerm_subnet.jenkins-subnet.id
+    subnet_id                     = azurerm_subnet.jenkins_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Network Interface Security Group Association
-resource "azurerm_network_interface_security_group_association" "jenkins-nsg" {
-  network_interface_id      = azurerm_network_interface.jenkins-vmnic.id
-  network_security_group_id = azurerm_network_security_group.jenkins-vmnsg.id
+resource "azurerm_network_interface_security_group_association" "jenkins_nsg" {
+  network_interface_id      = azurerm_network_interface.jenkins_vmnic.id
+  network_security_group_id = azurerm_network_security_group.jenkins_vmnsg.id
 }
 
 # Virtual Machine
-resource "azurerm_virtual_machine" "jenkins-vm" {
-  name                  = "${var.prefix}-vm"
-  resource_group_name   = azurerm_resource_group.jenkins-rg.name
-  location              = azurerm_resource_group.jenkins-rg.location
+resource "azurerm_virtual_machine" "jenkins_vm" {
+  name                  = "${var.prefix}-${random_pet.resource_name.id}-vm"
+  resource_group_name   = azurerm_resource_group.jenkins_rg.name
+  location              = azurerm_resource_group.jenkins_rg.location
   vm_size               = "Standard_DS1_v2"
-  network_interface_ids = [azurerm_network_interface.jenkins-vmnic.id]
+  network_interface_ids = [azurerm_network_interface.jenkins_vmnic.id]
 
   delete_os_disk_on_termination = true
 
@@ -116,22 +122,21 @@ resource "azurerm_virtual_machine" "jenkins-vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
-  
+
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "${random_pet.resource_name.id}-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
-    
   }
 
   os_profile {
-    computer_name  = "hostname"
+    computer_name  = "${random_pet.resource_name.id}-hostname"
     admin_username = var.admin_username
-    custom_data = file("jenkins.sh")
+    custom_data    = file("jenkins.sh")
   }
 
   os_profile_linux_config {
-    disable_password_authentication = true
+    disable_password_authentication = false
   }
 }
